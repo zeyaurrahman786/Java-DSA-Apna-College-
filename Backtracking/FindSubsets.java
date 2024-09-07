6e1748ee76c5704909f40f1d480b3d0ac4f1c750
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-09-07 15:18:29 +0530

    Find Subsets

---
+++ Backtracking/FindSubsets.java
@@ -0,0 +1,27 @@
+public class FindSubsets {
+
+    public static void findSubsets(String str, String ans, int i){
+        // base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        // Recursive case
+        
+        // Yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+
+        // No choice
+        findSubsets(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, "", 0);
+    }
+}

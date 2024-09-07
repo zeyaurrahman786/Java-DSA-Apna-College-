5cbcfadbe036abbbb4e2a9a8117eed59c6c101d6
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-09-07 13:41:42 +0530

    Backtracking

---
+++ Arrays2D/SearchInSortedMatrix.java
@@ -0,0 +1,5 @@
+public class SearchInSortedMatrix {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Backtracking/BtOnArrays.java
@@ -0,0 +1,30 @@
+// Backtracking on Arrays
+
+public class BtOnArrays {
+
+    public static void changeArr(int arr[], int i, int val) {
+        // Base case
+        if (i == arr.length) {
+            printArr(arr);
+            return;
+        }
+
+        // Recursion
+        arr[i] = val;
+        changeArr(arr, i + 1, val + 1); // function call step
+        arr[i] = arr[i] - 2; // backtracking step
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
